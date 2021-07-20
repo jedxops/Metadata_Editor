1217e9dc1069500fabde416efcd0ede7c3ee38f2
Jeff <zombitroid@gmail.com>
2021-07-20 16:37:53 -0700

    Added the script that compares the output of quotes vs without quotes for troubleshooting

---
+++ In Quotes VS WO Quotes.ps1
@@ -0,0 +1,10 @@
+﻿# Get paths
+$files_to_copy_metadata_to    = Get-ChildItem c:\Users\zombi\Desktop\originals
+$files_to_copy_metadata_from  = Get-ChildItem c:\Users\zombi\Desktop\copies
+
+ForEach($file1 in $files_to_copy_metadata_to) {
+  ForEach($file2 in $files_to_copy_metadata_from) {
+    Write-Host "Without quotes: " $file1.name $file2.Name
+    Write-Host "Versions in quotes: " "$file1.name" " " "$file2.name"
+  }
+}
